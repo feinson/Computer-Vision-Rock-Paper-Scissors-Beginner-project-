bd14b7457df72d256d07ce46d3be4be4aeadf3d3
Charles Feinson <charlie@feinson.com>
2022-11-09 22:41:36 +0000

    I believe that I have finished this one now but I guess we'll see

--- manual_rps.py
+++ manual_rps.py
@@ -6,7 +6,7 @@ def get_computer_choice():
     return random.choice(list_of_moves)
 
 def show_please_enter():
-    print("Please enter either 1, 2 or 3.")
+    print("Please enter either 1, 2 or 3.\n")
 
 def get_user_choice():
 
@@ -19,9 +19,10 @@ def get_user_choice():
         user_input = input("Please enter your choice.\n")
 
         try:
-            user_choice = int(user_input)
-
-            if user_choice in [1,2,3]:
+            
+            user_input = int(user_input)
+            
+            if user_input in [1,2,3]:
                 break
             
             else:
@@ -30,7 +31,29 @@ def get_user_choice():
         except:
             show_please_enter()
         
+
+    return list_of_moves[user_input-1]
+
+
+def get_winner(computer_choice, user_choice):
+
+    hierarchy = ["Rock", "Scissors", "Paper", "Rock"]
+
+    if computer_choice == user_choice:
+        print("It is a tie!")
+        return
+
+    elif hierarchy.index(computer_choice) + 1 == hierarchy.index(user_choice,1):
+        print("You lost")
+        return computer_choice
+        
+    else:
+        print("You won!")
         return user_choice
 
-print(get_computer_choice())
-print(get_user_choice())
+# a=get_computer_choice()
+
+# b=get_user_choice()
+# print(a,b)
+
+# get_winner(a,b)
