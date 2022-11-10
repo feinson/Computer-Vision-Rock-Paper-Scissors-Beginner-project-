c7d8f6a20c4b8967888e00754fcbba0a3888cff7
Charles Feinson <charlie@feinson.com>
2022-11-10 02:43:18 +0000

    the camera_rps.py file now shows the user a view of themselves for around 5 secs and when the timer is zero it takes that image as input

--- camera_rps.py
+++ camera_rps.py
@@ -1,11 +1,13 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-while True: 
+x=time.time()
+while (time.time()-x)<7:
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
     image_np = np.array(resized_frame)
@@ -13,12 +15,14 @@ while True:
     data[0] = normalized_image
     prediction = model.predict(data)
 
+    count_down = str(7-np.floor(time.time()-x))
+    cv2.putText(frame, count_down, (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),3)
     cv2.imshow('frame', frame)
+    
     # Press q to close the window
-    #print(prediction)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+         break
             
 # After the loop release the cap object
 cap.release()
