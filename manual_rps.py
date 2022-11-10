1e949ecc951641bc95c5cc495e8c2e47e0e9c151
Charles Feinson <charlie@feinson.com>
2022-11-10 01:04:02 +0000

    implemented game logic and switched to OOP

--- manual_rps.py
+++ manual_rps.py
@@ -1,63 +1,63 @@
 import random
-list_of_moves = ["Rock", "Paper", "Scissors"]
-
-def get_computer_choice():
-
-    return random.choice(list_of_moves)
 
 
-def show_please_enter():
+class rps:
 
-    print("Please enter either 1, 2 or 3.\n")
+    def __init__(self):
+        self.list_of_moves = ["Rock", "Paper", "Scissors"]
+        self.computer_choice = random.choice(self.list_of_moves)
+        self.user_choice = self.get_user_choice()
 
+    def show_please_enter():
 
-def get_user_choice():
+        print("Please enter either 1, 2 or 3.\n")
 
-    while True:
-        print("To select a move press:\n")
+    def get_user_choice(self):
 
-        for count, move in enumerate(list_of_moves):
-            print(f"{count+1} => {move}")
+        while True:
+            print("To select a move press:\n")
 
-        user_input = input("Please enter your choice.\n")
+            for count, move in enumerate(self.list_of_moves):
+                print(f"{count+1} => {move}")
 
-        try:
+            user_input = input("Please enter your choice.\n")
+
+            try:
+                user_input = int(user_input)
+                
+                if user_input in [1,2,3]:
+                    break
+                
+                else:
+                    rps.show_please_enter()
+
+            except:
+                rps.show_please_enter()
             
-            user_input = int(user_input)
+
+        return self.list_of_moves[user_input-1]
+
+
+    def get_winner(computer_choice, user_choice):
+
+        print(f"The computer chose {computer_choice} and you chose {user_choice}.\n")
+
+        hierarchy = ["Rock", "Scissors", "Paper", "Rock"]
+
+        if computer_choice == user_choice:
+            print("It is a tie!")
+            return
+
+        elif hierarchy.index(computer_choice) + 1 == hierarchy.index(user_choice,1):
+            print("You lost")
+            return computer_choice
             
-            if user_input in [1,2,3]:
-                break
-            
-            else:
-                show_please_enter()
+        else:
+            print("You won!")
+            return user_choice
 
-        except:
-            show_please_enter()
-        
+def play():
+    game = rps()
+    rps.get_winner(game.computer_choice, game.user_choice)
 
-    return list_of_moves[user_input-1]
-
-
-def get_winner(computer_choice, user_choice):
-
-    hierarchy = ["Rock", "Scissors", "Paper", "Rock"]
-
-    if computer_choice == user_choice:
-        print("It is a tie!")
-        return
-
-    elif hierarchy.index(computer_choice) + 1 == hierarchy.index(user_choice,1):
-        
-        print("You lost")
-        return computer_choice
-        
-    else:
-        print("You won!")
-        return user_choice
-
-# a=get_computer_choice()
-
-# b=get_user_choice()
-# print(a,b)
-
-# get_winner(a,b)
+play()
